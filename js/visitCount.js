cffb5d647c6d71e3b994c8fe864d6023dfdc7a60
user <dafu1985>
2024-02-28 10:04:49 +0900

    フッターにカウンタ追加

---
+++ js/visitCount.js
@@ -0,0 +1,22 @@
+<!-- 訪問者カウント用のスクリプト -->
+<script>
+    //カウントをローカルストレージから取得
+    let visitCount = localStorage.getItem('visitCount');
+
+    //カウントが存在しない場合は初期化
+    if(visitCount === null) {
+       visitCount = 0 ;
+    } else {
+    //文字列から数値に変換
+       visitCount = parseInt(visitCount);
+    }
+
+    //カウントを増やす
+    visitCount++ ;
+    
+    //カウントをローカルストレージに保存
+    localStorage.setItem('visitCount', visitCount);
+    
+    //カウントをHTMLに表示
+    document.getElementById('visitCount').textContent = visitCount;
+ </script>
