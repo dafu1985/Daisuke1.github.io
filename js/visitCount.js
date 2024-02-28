4818dc5967b593316839f0cd0b1a68c730dc51ca
user <dafu1985>
2024-02-28 11:28:32 +0900

    カウンター修正

--- js/visitCount.js
+++ js/visitCount.js
@@ -4,7 +4,7 @@
     //カウントをローカルストレージから取得
     let visitCount = localStorage.getItem('visitCount');
     
-    console.log('Visit count from local storage:', visitCount);
+  
 
     //カウントが存在しない場合は初期化
     if(visitCount === null) {
@@ -14,21 +14,21 @@
        visitCount = parseInt(visitCount);
     }
 
-console.log('Visit count before:', visitCount);
+
 
 // カウントを増やす
 visitCount++;
 
-console.log('Visit count after increment:', visitCount);
+
 
 // カウントをローカルストレージに保存
 localStorage.setItem('visitCount', visitCount);
 
-console.log('Visit count after saving to localStorage:', visitCount);
+
 
 // カウントをHTMLに表示
 document.getElementById('visitCount').textContent = visitCount;
 
-console.log('Visit count after updating HTML:', visitCount);
+
 
 </script>
