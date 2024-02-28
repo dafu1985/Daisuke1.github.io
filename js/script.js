81e73aaad61bf5af2f36cec1484736e19a8be797
user <dafu1985>
2024-02-28 11:09:41 +0900

    フッターにカウンタ追加5

--- js/script.js
+++ js/script.js
@@ -38,17 +38,3 @@ $(function () {
     return false;
   });
 });
-
- // カウントアップのスクリプト
-  let visitCount = localStorage.getItem('visitCount');
-
-  if (visitCount === null) {
-    visitCount = 0;
-  } else {
-    visitCount = parseInt(visitCount);
-  }
-
-  visitCount++;
-  localStorage.setItem('visitCount', visitCount);
-  document.getElementById('visitCount').textContent = visitCount;
-});
